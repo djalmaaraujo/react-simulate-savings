8afc9f3095d3f775f274406920a3a744f9e553a4
Djalma Araujo <djalma@close.io>
2018-10-06 21:19:41 +0100

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Simulate Savings
-
-Experimento para simular o rendimento da poupança em um período de meses, incluindo um aporte mensal.
-
-![image](https://raw.githubusercontent.com/djalmaaraujo/react-simulate-savings/gh-pages/screenshot.png)
---
+++ precache-manifest.01286f3c248e511427701c6e2ad5d8e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a8bc36bef1d3e44173",
+    "url": "/react-simulate-savings/static/js/runtime~main.e2a8bc36.js"
+  },
+  {
+    "revision": "cf773fe3a0f550595568",
+    "url": "/react-simulate-savings/static/js/main.cf773fe3.chunk.js"
+  },
+  {
+    "revision": "58c99ccf9603eee7c832",
+    "url": "/react-simulate-savings/static/js/1.58c99ccf.chunk.js"
+  },
+  {
+    "revision": "1e21422fb27c03b707d666cb15094707",
+    "url": "/react-simulate-savings/index.html"
+  }
+];
